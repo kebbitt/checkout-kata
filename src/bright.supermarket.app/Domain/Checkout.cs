976f71e6a292c49bd376a8d3334679a845859aa2
Keith Ebbitt <keith.ebbitt@gmail.com>
2024-11-29 07:46:40 +0000

    Add barebones of apps to solution

---
+++ src/bright.supermarket.app/Domain/Checkout.cs
@@ -0,0 +1,13 @@
+﻿namespace Bright.Supermarket.App.Domain;
+internal class Checkout : ICheckout
+{
+    public void Scan(string item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/bright.supermarket.app/Domain/ICheckout.cs
@@ -0,0 +1,6 @@
+﻿namespace Bright.Supermarket.App.Domain;
+public interface ICheckout
+{
+    void Scan(string item);
+    int GetTotalPrice();
+}
